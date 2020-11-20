b2f5afbb761eed965eebc99d0a1759fd4cc35e3f
Samuel-Azumah <72462228+Samuel-Azumah@users.noreply.github.com>
2020-11-20 05:16:35 -0800

    Add files via upload

---
+++ home/home.js
@@ -0,0 +1,9 @@
+prompt("God day, you are welcome. Please enter your name");
+
+const button = document.querySelector('button');
+
+button.addEventListener('click', ()=>{window.location.assign("../product/product.html");
+
+
+}
+)
